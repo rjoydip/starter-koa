1b4361c926013744045267ed94c3cf198b4622d2
Joydip Roy <joydipand@gmail.com>
2024-10-14 23:10:32 +0530

    chore: skip db test for CI

--- test/db.test.ts
+++ test/db.test.ts
@@ -1,5 +1,6 @@
 import type { User } from '../src/types'
 import { faker } from '@faker-js/faker/locale/en'
+import { isCI } from 'std-env'
 import { afterEach, describe, expect, it, vi } from 'vitest'
 import {
   createUser,
@@ -43,7 +44,7 @@ describe('⬢ Validate db', () => {
     expect(await isDBUp()).toBeTruthy()
   })
 
-  it.sequential('● should fetch users', async () => {
+  it.skipIf(isCI)('● should fetch users', async () => {
     const users = await getUsers()
     expect(users[0]).toBeUndefined()
   })
